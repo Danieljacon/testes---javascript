9c896e98c3ab3e77476410a02813c3ccc5830f18
danieljacon <danielcamillo2020@gmail.com>
2021-11-23 17:19:16 -0300

    Javascript melhorado e bem reduzido

--- 1/e2/script.js
+++ 1/e2/script.js
@@ -2,19 +2,29 @@
 Escreva um programa que receba as coordenadas de dois triângulos (sempre dados por seus cantos superior esquerdo e inferior direito) e diz se eles tem uma intersecção. Se ela existir, o programa também deve informar sua área.
 */
 
-const retanguloUm = document.querySelector(".ret1");
+const e = document.querySelector(".ret1");
 const retanguloDois = document.querySelector(".ret2");
 
-function changeColor1() {
-  const altura = document.querySelector(".r1Height").value;
-  const largura = document.querySelector(".r1Width").value;
-  retanguloUm.style.width = `${altura}px`;
-  retanguloUm.style.height = `${largura}px`;
-}
+function changeRet(e) {
+  const teste = {
+    altura: document.querySelector(".r1Height").value,
+    largura: document.querySelector(".r1Width").value,
+    esquerda: document.querySelector(".r1Esquerda").value,
+    top: document.querySelector(".r1Top").value,
+  };
 
-function changeColor2() {
-  const altura = document.querySelector(".r2Height").value;
-  const largura = document.querySelector(".r2Width").value;
-  retanguloDois.style.width = `${altura}px`;
-  retanguloDois.style.height = `${largura}px`;
+  if (e === 1) {
+    const ret = document.querySelector(".ret1");
+    ret.style.width = `${teste.altura}px`;
+    ret.style.height = `${teste.largura}px`;
+    ret.style.left = `${teste.esquerda}%`;
+    ret.style.top = `${teste.top}%`;
+  } 
+  else if (e === 2) {
+    const ret = document.querySelector(".ret2");
+    ret.style.width = `${teste.altura}px`;
+    ret.style.height = `${teste.largura}px`;
+    ret.style.left = `${teste.esquerda}%`;
+    ret.style.top = `${teste.top}%`;
+  }
 }
